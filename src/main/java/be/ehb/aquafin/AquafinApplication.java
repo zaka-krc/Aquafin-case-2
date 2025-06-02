52a24a85e6fe4a36772a3cf8d85a1576e30b23a7
zaka-krc <zakariakorchi2003@hotmail.com>
2025-06-02 14:13:53 +0200

    Initial commit

---
+++ src/main/java/be/ehb/aquafin/AquafinApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.aquafin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquafinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquafinApplication.class, args);
+	}
+
+}
