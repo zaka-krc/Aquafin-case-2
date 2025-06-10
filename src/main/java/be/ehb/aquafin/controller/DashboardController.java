a42a495f8ab53217035463b34813171d80f59f22
SoufianeAbk <soufiane.abakkiou@student.ehb.be>
2025-06-10 16:13:26 +0200

    Aanmaak van de dashboard pagina met de CSS en basis van HTML structuur.

---
+++ src/main/java/be/ehb/aquafin/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package be.ehb.aquafin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "dashboard";
+    }
+}
