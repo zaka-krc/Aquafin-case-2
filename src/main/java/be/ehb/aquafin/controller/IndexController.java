f07a747ca395217cfd126dce36eacbe789c0efc1
Sabrimrn <sabri.merroun@gmail.com>
2025-06-10 16:00:48 +0200

    MaandDAO Lists toegevoegd IndexController: code toegevoegd, alle data ophalen en seizoenstotalen berekenen

--- src/main/java/be/ehb/aquafin/controller/IndexController.java
+++ src/main/java/be/ehb/aquafin/controller/IndexController.java
@@ -1,4 +1,39 @@
 package be.ehb.aquafin.controller;
 
+import be.ehb.aquafin.model.Maand;
+import be.ehb.aquafin.model.MaandDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class IndexController {
+
+    @Autowired
+    private MaandDAO neerslagRepo;
+
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    public String showIndex(ModelMap map) {
+
+        // Haal alle data op
+        List<Maand> alleData = neerslagRepo.findAllByOrderByJaarDescMaandAsc();
+
+        // Bereken seizoenstotalen
+        Map<String, Double> seizoenTotalen = new HashMap<>();
+
+        for (Maand m : alleData) {
+            String key = m.getJaar() + " " + m.getSeizoen();
+            seizoenTotalen.merge(key, m.getNeerslag(), Double::sum);
+        }
+
+        map.addAttribute("seizoen_totalen", seizoenTotalen);
+
+        return "index";
+    }
 }
--- src/main/java/be/ehb/aquafin/model/MaandDAO.java
+++ src/main/java/be/ehb/aquafin/model/MaandDAO.java
@@ -1,6 +1,14 @@
 package be.ehb.aquafin.model;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MaandDAO extends CrudRepository<Maand, Integer> {
+
+    List<Maand> findByJaarOrderByMaand(int jaar);
+
+    List<Maand> findAllByOrderByJaarDescMaandAsc();
+
+    Maand findByJaarAndMaand(int jaar, int maand);
 
 }
