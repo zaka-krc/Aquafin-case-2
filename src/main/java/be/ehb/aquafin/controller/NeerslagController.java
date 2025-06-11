81185f9b5409b4aefd7d12a6b117ce784a417037
Sabrimrn <sabri.merroun@gmail.com>
2025-06-11 14:52:18 +0200

    Error gefixed van Neerslagcontroller

--- src/main/java/be/ehb/aquafin/controller/NeerslagController.java
+++ src/main/java/be/ehb/aquafin/controller/NeerslagController.java
@@ -2,14 +2,11 @@ package be.ehb.aquafin.controller;
 
 import be.ehb.aquafin.Service.NeerslagService;
 import be.ehb.aquafin.model.Maand;
-import be.ehb.aquafin.model.MaandDAO;
-import ch.qos.logback.core.model.Model;
-import jakarta.validation.Valid;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import java.util.List;
 import java.util.Map;
