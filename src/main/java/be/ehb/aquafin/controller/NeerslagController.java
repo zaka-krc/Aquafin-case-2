9a4295c16bff9b25004ac2e69fa3a2f32b3c813f
Sabrimrn <sabri.merroun@gmail.com>
2025-06-20 11:00:22 +0200

    NeerslagController geupdate (Dit is Anas die dit commit, want mijn github werkt niet op mijn laptop)

--- src/main/java/be/ehb/aquafin/controller/NeerslagController.java
+++ src/main/java/be/ehb/aquafin/controller/NeerslagController.java
@@ -17,6 +17,11 @@ public class NeerslagController {
     @Autowired
     private NeerslagService service;
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("/")
     public String index() {
         return "redirect:/voorspelling";
